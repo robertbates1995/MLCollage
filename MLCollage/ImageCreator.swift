33b8c682fa92c89627a0ec0b8df6620c69bd7a75
Robert Bates <thegreatbobini@gmail.com>
2024-07-18 11:22:06 -0700

    Implementing ImageCreator

--- MLCollage/ImageCreator.swift
+++ MLCollage/ImageCreator.swift
@@ -8,7 +8,51 @@
 import Foundation
 import Cocoa
 
-class ImageCreator: NSViewController {
-    var forestImage = NSImage(resource: .forest)
-    var monkeImage = NSImage(resource: .monke)
+class ImageCreator {
+    var background: NSImage
+    var subject: NSImage
+    
+    init(background: NSImage, subject: NSImage) {
+        self.background = background
+        self.subject = subject
+    }
+    
+    func createImage() -> NSImage {
+        return NSImage.fromCIImage(subject.ciImage()!.composited(over: background.ciImage()!))
+    }
+    
+    func move() {
+        
+    }
+    
+    func changeSize() {
+        
+    }
+    
+    func rotate() {
+        
+    }
+}
+
+extension NSImage {
+    /// Generates a CIImage for this NSImage.
+    /// - Returns: A CIImage optional.
+    func ciImage() -> CIImage? {
+        guard let data = self.tiffRepresentation,
+              let bitmap = NSBitmapImageRep(data: data) else {
+            return nil
+        }
+        let ci = CIImage(bitmapImageRep: bitmap)
+        return ci
+    }
+    
+    /// Generates an NSImage from a CIImage.
+    /// - Parameter ciImage: The CIImage
+    /// - Returns: An NSImage optional.
+    static func fromCIImage(_ ciImage: CIImage) -> NSImage {
+        let rep = NSCIImageRep(ciImage: ciImage)
+        let nsImage = NSImage(size: rep.size)
+        nsImage.addRepresentation(rep)
+        return nsImage
+    }
 }
--- MLCollage/InputsView.swift
+++ MLCollage/InputsView.swift
@@ -8,16 +8,16 @@
 import SwiftUI
 
 struct InputsView: View {
-    @State private var imageCreator = ImageCreator()
+    @State private var imageCreator = ImageCreator(
+        background: NSImage(resource: .forest),
+        subject: NSImage(resource: .monke)
+    )
     
     var body: some View {
         VStack {
             Text("Inputs View")
             ZStack {
-                Image(nsImage: imageCreator.forestImage)
-                    .resizable()
-                    .scaledToFit()
-                Image(nsImage: imageCreator.monkeImage)
+                Image(nsImage: imageCreator.createImage())
                     .resizable()
                     .scaledToFit()
             }
