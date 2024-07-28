887a0d9dc3cf734ba065e422f9a06f3d78ab0072
Robert Bates <thegreatbobini@gmail.com>
2024-07-27 22:53:09 -0700

    preparing for JSON creation

---
+++ MLCollage/Models/JSONEncoder.swift
@@ -0,0 +1,13 @@
+//
+//  JSONEncoder.swift
+//  MLCollage
+//
+//  Created by Robert Bates on 7/27/24.
+//
+
+import Foundation
+
+struct JSONEncoder: Codable {
+    
+    let image: String
+}
--- MLCollage/Models/SubjectData.swift
+++ MLCollage/Models/SubjectData.swift
@@ -9,17 +9,14 @@ import Foundation
 
 struct SubjectData {
     var label: String = ""
-    var coordinates: Coordinates
+    var x: Double = 0
+    var y: Double = 0
+    var width: Double = 0 //should be calculated based off image
+    var height: Double = 0 //should be calculated based off image
     
-    init(label: String, coordinates: Coordinates) {
+    init(label: String) {
         self.label = label
-        self.coordinates = coordinates
     }
     
-    class Coordinates {
-        var x: Double = 0
-        var y: Double = 0
-        var width: Double = 0 //should be calculated based off image
-        var height: Double = 0 //should be calculated based off image
-    }
+   
 }
