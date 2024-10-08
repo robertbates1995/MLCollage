869db2388b67bc6c3d10c690a4662624a06d2975
Robert Bates <thegreatbobini@gmail.com>
2024-10-08 11:13:09 -0700

    consolidated SliderView

--- MLCollage/Views/SliderView.swift
+++ MLCollage/Views/SliderView.swift
@@ -9,6 +9,57 @@
 import SwiftUI
 import Combine
 
+struct SliderView: View {
+    @ObservedObject var slider: CustomSlider
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: slider.lineWidth)
+            .fill(Color.gray.opacity(0.2))
+            .frame(width: slider.width, height: slider.lineWidth)
+            .overlay(
+                ZStack {
+                    //Path between both handles
+                    SliderPathBetweenView(slider: slider)
+                    
+                    //Low Handle
+                    SliderHandleView(handle: slider.lowHandle)
+                        .highPriorityGesture(slider.lowHandle.sliderDragGesture)
+                    
+                    //High Handle
+                    SliderHandleView(handle: slider.highHandle)
+                        .highPriorityGesture(slider.highHandle.sliderDragGesture)
+                }
+            )
+    }
+}
+
+struct SliderHandleView: View {
+    @ObservedObject var handle: SliderHandle
+    
+    var body: some View {
+        Circle()
+            .frame(width: handle.diameter, height: handle.diameter)
+            .foregroundColor(.white)
+            .shadow(color: Color.black.opacity(0.15), radius: 8, x: 0, y: 0)
+            .scaleEffect(handle.onDrag ? 1.3 : 1)
+            .contentShape(Rectangle())
+            .position(x: handle.currentLocation.x, y: handle.currentLocation.y)
+    }
+}
+
+struct SliderPathBetweenView: View {
+    @ObservedObject var slider: CustomSlider
+    
+    var body: some View {
+        Path { path in
+            path.move(to: slider.lowHandle.currentLocation)
+            path.addLine(to: slider.highHandle.currentLocation)
+        }
+        .stroke(Color.green, lineWidth: slider.lineWidth)
+    }
+}
+
+
 class CustomSlider: ObservableObject {
     
     //Slider Size
