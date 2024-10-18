06f21a0a0bc458826b32630b672ec56a80e319d5
Robert Bates <thegreatbobini@gmail.com>
2024-10-18 11:39:09 -0700

    added path between and prevented crossover

--- MLCollage/Views/HighLowSlider.swift
+++ MLCollage/Views/HighLowSlider.swift
@@ -15,7 +15,7 @@ struct HighLowSlider: View {
     @Binding var highValue: Double
     @Binding var lowValue: Double
     @State var lowDrag: Bool = false
-    
+    @State var highDrag: Bool = false
     
     var body: some View {
         GeometryReader { proxy in
@@ -26,13 +26,26 @@ struct HighLowSlider: View {
                     .frame(height: 4.0)
                     .overlay(
                         ZStack {
-                            HandleView(diameter: 28, onDrag: false, currentLocation:  CGPoint(x: lowValue * proxy.size.width, y: 2))
+                            PathBetweenView(lineWidth: 4.0, lowValue: location(lowValue, proxy), highValue: location(highValue, proxy))
+                            //low handle
+                            HandleView(diameter: 28, onDrag: lowDrag, at: location(lowValue, proxy))
                                 .highPriorityGesture(DragGesture()
                                     .onChanged { value in
                                         self.lowDrag = true
-                                        self.lowValue = value.location.x / proxy.size.width
+                                        let tempValue = value.location.x / proxy.size.width
+                                        self.lowValue = min(tempValue, highValue)
                                     }.onEnded { _ in
                                         self.lowDrag = false
+                                    })
+                            //high handle
+                            HandleView(diameter: 28, onDrag: highDrag, at:  location(highValue, proxy))
+                                .highPriorityGesture(DragGesture()
+                                    .onChanged { value in
+                                        self.highDrag = true
+                                        let tempValue = value.location.x / proxy.size.width
+                                        self.highValue = max(tempValue, lowValue)
+                                    }.onEnded { _ in
+                                        self.highDrag = false
                                     })
                         }
                     )
@@ -40,12 +53,30 @@ struct HighLowSlider: View {
             }
         }
     }
+    
+    func location(_ value: Double, _ proxy: GeometryProxy) -> CGPoint {
+        CGPoint(x: (value * proxy.size.width), y: 2)
+    }
+}
+
+fileprivate struct PathBetweenView: View {
+    let lineWidth: CGFloat
+    let lowValue: CGPoint
+    let highValue: CGPoint
+    
+    var body: some View {
+        Path { path in
+            path.move(to: lowValue)
+            path.addLine(to: highValue)
+        }
+        .stroke(Color.blue, lineWidth: lineWidth)
+    }
 }
 
 fileprivate struct HandleView: View {
     let diameter: Double
     let onDrag: Bool
-    let currentLocation: CGPoint
+    let at: CGPoint
     
     var body: some View {
         Circle()
@@ -54,7 +85,7 @@ fileprivate struct HandleView: View {
             .shadow(color: Color.black.opacity(0.15), radius: 8, x: 0, y: 0)
             .scaleEffect(onDrag ? 1.3 : 1)
             .contentShape(Rectangle())
-            .position(currentLocation)
+            .position(at)
     }
 }
 
