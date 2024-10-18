80df4d86fde72601af690d160d1ee7b0996605f5
Robert Bates <thegreatbobini@gmail.com>
2024-10-18 12:23:16 -0700

    made two slider views

--- MLCollage/Views/SettingsView.swift
+++ MLCollage/Views/SettingsView.swift
@@ -13,45 +13,50 @@ struct SettingsView: View {
     
     var body: some View {
         List {
-            SettingView(title: "population", value: $settings.population, upperValue: $settings.population, range: 0...100, hasLower: false)
-            SettingView(title: "number of each subject", value: $settings.numberOfEachSubject, upperValue: $settings.numberOfEachSubject, range: 0...20, hasLower: false)
-            SettingView(title: "translation", value: $settings.translateLowerBound, upperValue: $settings.translateUpperBound, range: 0...100, hasLower: true)
+            SliderView(title: "population", value: $settings.population, range: 0...100)
+            SliderView(title: "number of each subject", value: $settings.numberOfEachSubject, range: 0...20)
+            HighLowSliderView(title: "translation", value: $settings.translateLowerBound, upperValue: $settings.translateUpperBound, range: 0...100)
         }.scrollDisabled(true)
     }
 }
 
-struct SettingView: View {
+struct HighLowSliderView: View {
     let title: String
     @Binding var value: Double
     @Binding var upperValue: Double
     let range: ClosedRange<Double>
-    let hasLower: Bool
     
     var body: some View {
-        if hasLower {
-            Section(title) {
-                HStack {
-                    Text("Lower Value: ")
-                    Text(String(format: "%g", value))
-                    Spacer()
-                    Text("Upper Value: ")
-                    Text(String(format: "%g", upperValue))
-                }
-                VStack {
-                    HighLowSlider(highValue: $upperValue, lowValue: $value, range: range)
-                }
+        Section(title) {
+            HStack {
+                Text("Lower Value: ")
+                Text(String(format: "%g", value))
+                Spacer()
+                Text("Upper Value: ")
+                Text(String(format: "%g", upperValue))
             }
-        } else {
-            Section(title) {
-                HStack {
-                    Text(String(format: "%g", value.rounded()))
-                }
-                Slider(value: $value, in: range) {
-                    Text("population")
-                } onEditingChanged: { _ in
-                    value = value.rounded()
-                    print("\(value)")
-                }
+            VStack {
+                HighLowSlider(highValue: $upperValue, lowValue: $value, range: range)
+            }
+        }
+    }
+}
+
+struct SliderView: View {
+    let title: String
+    @Binding var value: Double
+    let range: ClosedRange<Double>
+    
+    var body: some View {
+        Section(title) {
+            HStack {
+                Text(String(format: "%g", value.rounded()))
+            }
+            Slider(value: $value, in: range) {
+                Text("population")
+            } onEditingChanged: { _ in
+                value = value.rounded()
+                print("\(value)")
             }
         }
     }
