b194979216db11237e50ef84664253c75a1b5358
Robert Bates <thegreatbobini@gmail.com>
2024-11-15 19:58:07 -0800

    starting to add tests

--- MLCollageTests/CollageCreatorTests.swift
+++ MLCollageTests/CollageCreatorTests.swift
@@ -10,6 +10,17 @@ import XCTest
 import CustomDump
 @testable import MLCollage
 
+@MainActor
 final class CollageTests: XCTestCase {
+    let inputModel: InputModel = .init(subjects: ["apple":.init(label: <#T##String#>, images: <#T##[UIImage]#>)], backgrounds: <#T##[UIImage]#>)
+    let outputModel: OutputModel = .init(collages: [Collage(image: <#T##UIImage#>, json: <#T##CreateMLFormat#>)], factories: [])
     
+    func testOutputFile() {
+        var appleImages = [CIImage(image: .apple1)!.toUIImage(),
+                           CIImage(image: .apple2)!.toUIImage(),
+                           CIImage(image: .apple3)!.toUIImage()]
+        let appleSubject = Subject(label: "apple", images: appleImages)
+        
+        let outputModel = OutputModel(collages: <#T##[Collage]#>, factories: <#T##[CollageBlueprint]#>)
+    }
 }
