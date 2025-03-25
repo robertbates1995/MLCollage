f6db9add44901647baadf6ef1839c1761fa842ea
Robert Bates <thegreatbobini@gmail.com>
2025-03-25 14:56:47 -0700

    improved backgrounds view

--- MLCollage/Views/BackgroundsView.swift
+++ MLCollage/Views/BackgroundsView.swift
@@ -13,29 +13,35 @@ struct BackgroundsView: View {
 
     var body: some View {
         NavigationView {
-            LazyVGrid(
-                columns: [ GridItem(.flexible()),
-                           GridItem(.flexible()),],
-                spacing: 10
-            ) {
-                ForEach(model.backgrounds) { image in  // Replace with your data model here
-                    Image(uiImage: image.uiImage)
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(
-                            minWidth: 0, maxWidth: .infinity, minHeight: 0,
-                            maxHeight: .infinity, alignment: .center
-                        )
-                        .aspectRatio(1 / 1, contentMode: .fit)
-                        .clipped()
-                        .mask {
-                            RoundedRectangle(
-                                cornerRadius: 6, style: .continuous)
+            List {
+                ZStack {
+                    LinearGradient(gradient: Gradient(colors: [.black.opacity(0.05), .black.opacity(0)]), startPoint: .top, endPoint: .bottom)
+                    LazyVGrid(
+                        columns: [
+                            GridItem(.flexible()),
+                            GridItem(.flexible()),
+                        ],
+                        spacing: 10
+                    ) {
+                        ForEach(model.backgrounds) { image in
+                            Image(uiImage: image.uiImage)
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(
+                                    minWidth: 0, maxWidth: .infinity, minHeight: 0,
+                                    maxHeight: .infinity, alignment: .center
+                                )
+                                .aspectRatio(1 / 1, contentMode: .fit)
+                                .clipped()
+                                .mask {
+                                    RoundedRectangle(
+                                        cornerRadius: 6, style: .continuous)
+                                }
                         }
+                    }
                 }
             }
-            .padding()
             .navigationTitle("Backgrounds")
         }
     }
