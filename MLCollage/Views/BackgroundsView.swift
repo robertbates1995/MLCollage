bda101639517272da7c2f08e67f1b408d3409128
Robert Bates <thegreatbobini@gmail.com>
2025-03-25 14:19:20 -0700

    improved backgroundsView

--- MLCollage/Views/BackgroundsView.swift
+++ MLCollage/Views/BackgroundsView.swift
@@ -8,14 +8,34 @@
 import Foundation
 import SwiftUI
 
-
 struct BackgroundsView: View {
     @Binding var model: InputModel
 
-    
     var body: some View {
         NavigationView {
-            
+            LazyVGrid(
+                columns: [ GridItem(.flexible()),
+                           GridItem(.flexible()),],
+                spacing: 10
+            ) {
+                ForEach(model.backgrounds) { image in  // Replace with your data model here
+                    Image(uiImage: image.uiImage)
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(
+                            minWidth: 0, maxWidth: .infinity, minHeight: 0,
+                            maxHeight: .infinity, alignment: .center
+                        )
+                        .aspectRatio(1 / 1, contentMode: .fit)
+                        .clipped()
+                        .mask {
+                            RoundedRectangle(
+                                cornerRadius: 6, style: .continuous)
+                        }
+                }
+            }
+            .padding()
         }
     }
 }
