7c6970f9c5323db1cdb5bbe03305e09cd37fb97c
Robert Bates <thegreatbobini@gmail.com>
2025-03-31 13:24:01 -0700

    added light and dark previews

--- MLCollage/Views/ContentView.swift
+++ MLCollage/Views/ContentView.swift
@@ -10,16 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State var visibility: NavigationSplitViewVisibility = .all
     @Binding var project: Project
-    
+    @State private var isDarkMode = false
+
     //splash screen variables
     @State var isActive = false
-    @State var size = 0.8
+    @State var size = 0.7
     @State var opacity = 0.5
-    
+
     var body: some View {
         if isActive {
             TabView {
-                Tab("Inputs", systemImage: "square.and.arrow.down.on.square") {
+                Tab("Subjects", systemImage: "square.and.arrow.down.on.square")
+                {
                     AllSubjectsView(model: $project.inputModel)
                 }
                 Tab("Backgrounds", systemImage: "photo") {
@@ -35,24 +37,24 @@ struct ContentView: View {
         } else {
             VStack {
                 VStack {
-                    Image(.mlCollageIconLight)
+                    Image(isDarkMode ? .mlCollageIconDark : .mlCollageIconLight)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                    Text("ML Collage")
-                        .font(.title)
                 }
-                .padding(120.0)
-                .scaleEffect(size)
-                .opacity(opacity)
-                .onAppear {
-                    withAnimation(.easeIn(duration: 1.2)) {
-                        self.size = 0.9
-                        self.opacity = 1.0
-                    }
+                Text("ML Collage")
+                    .font(.title)
+            }
+            .padding(120.0)
+            .scaleEffect(size)
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeIn(duration: 2.0)) {
+                    self.size = 0.9
+                    self.opacity = 1.0
                 }
             }
             .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
                     self.isActive = true
                 }
             }
@@ -62,5 +64,11 @@ struct ContentView: View {
 
 #Preview {
     @Previewable @State var model = Project.mock
-    ContentView(project: $model)
+    ContentView(project: $model).preferredColorScheme(.light)
+}
+
+#Preview {
+    @Previewable @State var model = Project.mock
+    ContentView(project: $model).preferredColorScheme(.dark)
+
 }
